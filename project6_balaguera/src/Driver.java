2835aa2ad1854073fb86b5043a709bb89bdfa653
Ian Balaguera <balagueraian@gmail.com>
2018-11-14 16:34:58 -0500

    Project 6
    βick better be hella happy now

--- project6_balaguera/src/Driver.java
+++ project6_balaguera/src/Driver.java
@@ -3,18 +3,41 @@ import java.util.Scanner;
 /**
  * Calculates a dual meet score between two teams from a source file.
  * @author Ian Balaguera
- * @version 11.13.18
+ * @version 11.14.18
  */
 public class Driver {
     public static void main(String[] args) throws Exception {
+        String[] teams = {"TABOR ACADEMY", "WESTMINSTER", "THAYER ACADEMY", "ROXBURY LATIN",
+                "GROTON", "WORCESTER ACADEMY", "HOPKINS", "SUFFIELD ACADEMY", "MIDDLESEX",
+                "NOBLES", "BERKSHIRE", "SALISBURY", "BB&N", "KENT", "THE GOVERNOR'S ACADEMY",};
         String fileName = "xc.txt";
         Scanner userIn = new Scanner(System.in);
 
-        System.out.print("First team: ");
-        String teamOne = userIn.nextLine();
+        String teamOne = "";
+        boolean realTeamOne = false;
+        while (!realTeamOne) {
+            System.out.print("First team: ");
+            teamOne = userIn.nextLine();
+            for (int i = 0; i < teams.length; i++) {
+                if (teamOne.trim().equalsIgnoreCase(teams[i])) {
+                    realTeamOne = true;
+                }
+            }
+            if (!realTeamOne) System.out.println("Invalid team name.");
+        }
 
-        System.out.print("Second team: ");
-        String teamTwo = userIn.nextLine();
+        String teamTwo = "";
+        boolean realTeamTwo = false;
+        while (!realTeamTwo) {
+            System.out.print("Second team: ");
+            teamTwo = userIn.nextLine();
+            for (int i = 0; i < teams.length; i++) {
+                if (teamTwo.trim().equalsIgnoreCase(teams[i])) {
+                    realTeamTwo = true;
+                }
+            }
+            if (!realTeamTwo) System.out.println("Invalid team name.");
+        }
 
         userIn.close();
 
